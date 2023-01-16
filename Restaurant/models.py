b7ab60f9fee9ee5617078aed2627c4b19b321264
santibenson <bensonsibigamisaac@gmail.com>
2023-01-16 23:27:10 +0100

    Basic config.

---
+++ Restaurant/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from datetime import datetime
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    date = models.DateTimeField(default=datetime.today())
+    
+
+    class Meta:
+        verbose_name = "Booking"
+        verbose_name_plural = "Bookings"
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    inventory = models.IntegerField()
+    
+
+    class Meta:
+        verbose_name = "Menu"
+        verbose_name_plural = "Menus"
+
+    def __str__(self):
+        return self.title
