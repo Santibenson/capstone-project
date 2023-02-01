8c1da9950c1eb0ec4a8ec5253e8eb9f6893f3f8c
santibenson <bensonsibigamisaac@gmail.com>
2023-02-01 23:17:46 +0100

    test_views modified and updated README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # capstone project
  Final project for Meta Back-End Development Professional. I'm task to solve the issues restaurants face during ordering, booking and reservation.
+
+# Paths to test
+restaurant/api/menu
+restaurant/api/menu/pk
+restaurant/api/booking
+
+# Note
+It's token-based, you'd need to create an account with djoser
+/auth/users/
+
+# login
+/auth/token/login
--- Tests/test_views.py
+++ Tests/test_views.py
@@ -1,23 +1,22 @@
 
-from django.test import TestCase, Client
-from django.urls import reverse
+from django.test import TestCase, RequestFactory
 from rest_framework import status
 from Restaurant.models import Menu
 from API.serializers import MenuSerializer
-
+from API.views import MenuItemView
 
 
 class MenuViewTest(TestCase):
     def setUp(self):
-        self.client = Client()
-        Menu.objects.create(title="Milkshake", price="3000", inventory="10")
-        Menu.objects.create(title="Pineapple Juice", price="1500", inventory="15")
-        Menu.objects.create(title="Pina Colada", price="4000", inventory="5")
-        Menu.objects.create(title="Sex on the beach", price="3500", inventory="8")
+        self.factory = RequestFactory()
+
+        self.milkshake = Menu.objects.create(title="Milkshake", price=3000, inventory=10)
+        self.pineapple = Menu.objects.create(title="Pineapple Juice", price=3000, inventory=10)
 
     def test_getall(self):
-        response = self.client.get(reverse('menu'))
         menu_ = Menu.objects.all()
         serializer = MenuSerializer(menu_, many=True)
+        request = self.factory.get('restaurant/api/menu')
+        response = MenuItemView.as_view()(request)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.data, status.HTTP_200_OK)
